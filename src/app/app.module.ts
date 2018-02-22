24eb1d73cc65a63bd6b07342d849580e3b3f566f
KashaevaD <dasylya17199@gmail.com>
2018-02-22 20:55:07 +0200

    update styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,9 @@ import { AppComponent } from './app.component';
 import { AnimalsPageComponent } from './animals-page/animals-page.component';
 import { BirdsPageComponent } from './birds-page/birds-page.component';
 import { PageLayoutComponent } from './page-layout/page-layout.component';
-
-import { appRouter } from './app-routes';
-import { RowComponent } from './row/row.component';
 import { TimingComponent } from './timing/timing.component';
 
+import { appRouter } from './app-routes';
 
 @NgModule({
   declarations: [
@@ -20,7 +18,6 @@ import { TimingComponent } from './timing/timing.component';
     AnimalsPageComponent,
     BirdsPageComponent,
     PageLayoutComponent,
-    RowComponent,
     TimingComponent,
   ],
   imports: [
